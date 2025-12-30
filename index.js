d0ffdf98aa28e854c36b2b78ce182c6f2f33da74
mahdiebene <mmaahhddiieebbeennee@gmail.com>
2025-12-30 08:23:01 +0600

    Fix Render Startup: Explicitly start bot in index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-require('./dist/bot.js');
+const { BotEngine } = require('./dist/bot');
+
+// Initialize and start the bot
+const bot = new BotEngine();
+bot.start([
+    'https://www.effectivegatecpm.com/j7zbnug3?key=6d61e432f43f295a3df95b127be03643'
+]);
